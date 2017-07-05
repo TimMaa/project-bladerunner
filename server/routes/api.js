1b9f98b4a90c76d00c4ff1bbe3e7ef07a6ffc6a9
Markus <markus.boebel@hpe.com>
2017-07-05 16:14:45 +0200

    Bugfixed that shiet

--- server/routes/api.js
+++ server/routes/api.js
@@ -10,17 +10,4 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
-for(var i = 0; i<100;i++) {
-  for (var j = 0; j < 100; j++) {
-    data.setPoint(i, j, '#FF0000').subscribe(()=> console.log("erstellt"));
-  }
-}
-
-//data.getPoint(1000,10,(err, data) => console.log("danach :" + data));
-
-//data.getPoints().subscribe((data,err) => console.log(data));
-
-console.log(data.isTaken(1000,10));
-
-
 module.exports = router;
