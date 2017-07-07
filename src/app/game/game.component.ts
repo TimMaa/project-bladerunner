16c75ac5ffe0b0d9fe7d70126c6011ef79041350
Tim Maassen <tim.maassen@hpe.com>
2017-07-07 11:21:01 +0200

    Add Moveable And Zoomable Game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,58 +9,91 @@ export class GameComponent implements OnInit {
 
   canvasWidth: number;
   canvasHeight: number;
-  
-  posTop: number;
-  posLeft: number;
 
-  playerID: string;
+  posTop: number;
+  posRight: number;
+  posLeft: number;
+  posBottom: number;
+
+  movingInterval;
+
+  zoom: number = 1;
 
   constructor() {
   }
 
   ngOnInit() {
+    /**
+     * Set Canvas Starting Position (Top, Left) and Canvas Dimension (Height and Width) on Init
+     * Right and Bottom are for scrolling
+     */
     this.posTop = 0;
     this.posLeft = 0;
     this.canvasHeight = 2000;
     this.canvasWidth = 2000;
+    this.posRight = this.canvasWidth - window.innerWidth + this.posLeft;
+    this.posBottom = this.canvasHeight - window.innerHeight + this.posTop;
   }
 
-  getData() {
-    /*
-    let c = (<HTMLCanvasElement>document.getElementById('gameCanvas'));
-    let ctx = c.getContext('2d');
-
-    console.log(ctx.getImageData(0, 0, c.width, c.height).data);
+  changeZoom() {
+    /**
+     * To get an overview of the entire drawing, you can zoom
+     * Zoom should be proportional to the device size and dont change the ratio of the canvas
      */
+    // Zoom out when zoomed in (zoom = 1) and the other way around
+    if (this.zoom === 1) {
+      // Get necessary Zoomlevel
+      // Check which canvas dimension overlaps more and use this (-10% to have some margin)
+      this.zoom = 1 / (Math.max(this.canvasHeight / window.innerHeight, this.canvasWidth / window.innerHeight) + 0.1);
+      // Reposition the canvas so it is centered
+      this.posTop = (window.innerHeight - this.canvasHeight * this.zoom) / 2;
+      this.posLeft = (window.innerWidth - this.canvasWidth * this.zoom) / 2;
+    } else {
+      // Zoom back in
+      this.zoom = 1;
+      // Place the canvas back to the top-left
+      this.posTop = 0;
+      this.posLeft = 0;
+    }
   }
 
   moveWindow(element: number) {
-    let c = (<HTMLCanvasElement>document.getElementById('gameCanvas'));
-    let dh = document.body.clientHeight;
-    let dw = document.body.clientWidth;
-    let ct = c.offsetTop;
-    let cl = c.offsetLeft;
-    let cb = this.canvasHeight - dh + c.offsetTop;
-    let cr = this.canvasWidth - dw + c.offsetLeft;
+    let dh = window.innerHeight;
+    let dw = window.innerWidth;
+    this.posBottom = this.canvasHeight - dh + this.posTop;
+    this.posRight = this.canvasWidth - dw + this.posLeft;
+
     switch (element) {
       case 1:
-        if (ct < 0) {
-          this.posTop += 50;
+        if (this.posTop < 0) {
+          this.posTop += 20;
+        }
+        if (this.posTop > 0) {
+          this.posTop = 0;
         }
         break;
       case 2:
-        if (cl < 0) {
-          this.posLeft -= 50;
+        if (this.posRight > 0) {
+          this.posLeft -= 20;
+        }
+        if (this.posRight < 0) {
+          this.posLeft = -this.canvasWidth + dw;
         }
         break;
       case 3:
-        if (cb < 0) {
-          this.posTop -= 50;
+        if (this.posBottom > 0) {
+          this.posTop -= 20;
+        }
+        if (this.posBottom < 0) {
+          this.posTop = -this.canvasHeight + dh;
         }
         break;
       case 4:
-        if (cr < 0) {
-          this.posLeft += 50;
+        if (this.posLeft < 0) {
+          this.posLeft += 20;
+        }
+        if (this.posLeft > 0) {
+          this.posLeft = 0;
         }
         break;
       default:
@@ -70,18 +103,20 @@ export class GameComponent implements OnInit {
   }
 
   getMousePosition(event) {
-    let x = event.x;
-    let y = event.y;
+    if (this.zoom === 1) {
+      let x = event.x;
+      let y = event.y;
 
-    let c = (<HTMLCanvasElement>document.getElementById('gameCanvas'));
-    let ctx = c.getContext('2d');
+      let c = (<HTMLCanvasElement>document.getElementById('gameCanvas'));
+      let ctx = c.getContext('2d');
 
-    x -= c.offsetLeft;
-    y -= c.offsetTop;
+      x -= c.offsetLeft;
+      y -= c.offsetTop;
 
-    ctx.fillStyle = '#133769';
+      ctx.fillStyle = '#133769';
 
-    ctx.fillRect(Math.floor(x / 10) * 10, Math.floor(y / 10) * 10, 10, 10);
+      ctx.fillRect(Math.floor(x / 10) * 10, Math.floor(y / 10) * 10, 10, 10);
+    }
   }
 
 }
