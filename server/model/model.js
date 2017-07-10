c3b7728732fe4ab3f94f265ae6e4783db61594a3
Markus <markus.boebel@hpe.com>
2017-07-10 16:00:44 +0200

    Created REST API
    Points :
    
    GET | api/points   | Gets all points
    POST| api/points   | Sets Point      | Input: JSON with x,y,color
    GET | api/solution/|
           :solution   | Tests the answer| :solution is the answer

--- server/model/model.js
+++ server/model/model.js
@@ -5,6 +5,7 @@
  * Modul kann importiert werden mit:
  * var data = require(<Pfad zu dieser Datei>);
  */
+const Rx = require('rxjs/Rx');
 
 const contactPoints = ['192.168.99.100'];
 const keySpace = 'gameoflife';
@@ -13,7 +14,13 @@ const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({contactPoints: contactPoints, keyspace: keySpace});
 const validator = require('validator');
 
-const Rx = require('rx');
+
+/**
+ * Number of Words in Database
+ * @type {number}
+ */
+const wordCount = 3;
+
 
 
 var exports = module.exports = {};
@@ -29,42 +36,40 @@ function checkValues(x, y, color) {
     return null;
   }
   return true;
-
 }
-function doQuery(query) {
+
+exports.doQuery =  function(query) {
   return Rx.Observable.create(observer => {
     client.execute(query, {prepare: true}, function (err, result) {
       if (!err && result.rows !== undefined) {
         if (result.rows.length > 0) {
-          let user = result.rows;
-          observer.onNext(user);
-          observer.onError(err);
-          callback(err, user);
+          let data = result.rows;
+          observer.next(data);
         } else {
-          observer.onError("No items");
-          observer.onNext(null);
+          observer.error("No items");
         }
-      } else
-      {
-        observer.onError(err);
-        observer.onNext(null);
-      }
+      } else if(err){
+        observer.error(err);
+      }else observer.complete();
     });
-
-
-    observer.onCompleted("fertig");
   });
 }
 
+function getWordPositionByTime() {
+  let time = Date.now();
+  return Math.floor(Math.floor(time / 1000) / 300) % wordCount;
+};
 /**
  * Alle Punkte werden zurückgegeben
  *
  * @returns Observable, welches eine JSON Liste mit den Objekten zurückgibt
  */
 exports.getPoints = function () {
-    let query = 'SELECT * FROM coordinates';
-    return doQuery(query);
+  let query = 'SELECT * FROM coordinates';
+  return exports.doQuery(query);
 }
+
+
 /**
  * Punkt wird zurückgeben
  *
@@ -76,7 +81,7 @@ exports.getPoints = function () {
 exports.getPoint = function (x, y) {
   if (checkValues(x, y)) {
     let query = 'SELECT * FROM coordinates WHERE x = ' + x + ' AND y = ' + y;
-    return doQuery(query, (err, data));
+    return exports.doQuery(query, (err, data));
   }
 }
 
@@ -90,7 +95,11 @@ exports.getPoint = function (x, y) {
 exports.setPoint = function (x, y, color) {
   if (checkValues(x, y, color)) {
     let query = "INSERT INTO coordinates(x,y,color,time) values (" + x + "," + y + ",'" + color + "', toTimestamp(now()));";
-    return doQuery(query);
+    return exports.doQuery(query);
+  } else {
+    return Rx.Observable.create(observer => {
+      observer.error("Error");
+    });
   }
 }
 
@@ -103,15 +112,25 @@ exports.setPoint = function (x, y, color) {
 exports.deletePoint = function (x, y) {
   if (checkValues(x, y)) {
     let query = "DELETE FROM coordinates WHERE x =" + x + " AND y = " + y + ");";
-    return doQuery(query);
+    return exports.doQuery(query);
   }
 }
 
-/*
-TODO Funktion zum prüfen ob ein Punkt existiert
- */
-exports.isTaken = function (x, y) {
-  //return exports.getPoint(x,y,(err,data)=> {callback(return data instanceof Object});
+exports.getSolution = function (loesung, callback) {
+  let query = "SELECT word from words WHERE wordno = " + getWordPositionByTime();
+  let erg = exports.doQuery(query);
+
+  erg.subscribe(data => {
+      callback(data[0].word === loesung);
+    }
+    , err => callback(null));
+
+  /*
+   TODO Funktion zum prüfen ob ein Punkt existiert
+   */
+  exports.isTaken = function (x, y) {
+    //return exports.getPoint(x,y,(err,data)=> {callback(return data instanceof Object});
+  }
+
+
 }
-
-
--- server/routes/api.js
+++ server/routes/api.js
@@ -4,10 +4,75 @@
 
 const express = require('express');
 const router = express.Router();
+/**
+ * Datamodel
+ */
+const data = require('../model/model');
 
 /* GET api listing. */
 router.get('/', (req, res) => {
   res.send('api works');
+});
+
+/**
+ * REST ENDPOINT
+ * Endpoint to get all points set
+ *
+ * Sends Points in JSON format or ERR Message in JSON format
+ */
+router.get('/points/', (req, res) => {
+  data.getPoints().subscribe(
+    data => {
+      res.status(200);
+      res.send(data);
+    }
+  )
+});
+
+/**
+ * REST ENDPOINT
+ * Endpoint to set new Points
+ *
+ * Require JSON Object with x,y,color
+ */
+router.post('/points/', (req, res) => {
+  if (req.body) {
+    let body = req.body
+      , x = body.x
+      , y = body.y
+      , color = body.color;
+
+    if (x && y && color) {
+      data.setPoint(x, y, color).subscribe(
+        ()=>{},
+        err => {
+          res.status(400);
+          res.send(err);
+        },
+        () => {
+          res.status(200);
+          res.send("created");
+        });
+    } else {
+      res.status(400);
+      res.send("Wrong information sent");
+    }
+  }
+  else {
+    res.status(400);
+    res.send("Error: No body");
+  }
+});
+
+/**
+ *
+ */
+router.get('/solution/:solution', (req, res) => {
+  let solution = req.params.solution;
+  data.getSolution(solution, data => {
+    res.status(data === null ? 400 : 200);
+    res.send(data);
+  });
 });
 
 module.exports = router;
