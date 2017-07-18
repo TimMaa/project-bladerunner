b9cea88306147f7d56bec6d3fc49ff01b274a880
Luca Dommes <luca.dommes@hpe.com>
2017-07-18 12:02:44 +0200

    refactored function

--- server/bots/bots.js
+++ server/bots/bots.js
@@ -3,56 +3,53 @@
  * Bot for creating random points with random colors
  */
 
-const colors = [
-  '#000',
-  '#800',
-  '#F00',
-  '#F0F',
-  '#088',
-  '#080',
-  '#0F0',
-  '#0FF',
-  '#008',
-  '#808',
-  '#00F',
-  '#888',
-  '#880',
-  '#FF0',
-  '#FFF',
-  '#C0C0C0'
-];
-
-/**
- * @param max = biggest possible return value
- * @returns {number} random integer between 0 and max
- */
-let randomNumber = function (max) {
-  return Math.floor(Math.random() * max);
-};
-
 /**
  * When button is clicked bot starts creating random points and colors and
  * sends those to api/points/
  */
-$(document).ready(
-  function () {
-    const maxSize = 2000;
-    $('#bot').on('click', function () {
-      setInterval(function () {
-        let json = {
-          x: randomNumber(maxSize),
-          y: randomNumber(maxSize),
-          color: colors[randomNumber(colors.length)]
-        };
-        $.post('/api/points/', json, function (result) {
-          console.log(result);
-        }, 'application/json');
-        /*
-         $.get('/api/points', function (result) {
-         console.log(result);
-         })
-         */
-      }, 10) //TODO Intervall ggf. anpassen
-    });
-  }
-);
+function botstart(url,intervalTime,maxSize, post) {
+  const url = url || '/api/points/';
+  const intervalTime = intervalTime || 10;
+  const maxSize = maxSize || 2000;
+  const colors = [
+    '#000',
+    '#800',
+    '#F00',
+    '#F0F',
+    '#088',
+    '#080',
+    '#0F0',
+    '#0FF',
+    '#008',
+    '#808',
+    '#00F',
+    '#888',
+    '#880',
+    '#FF0',
+    '#FFF',
+    '#C0C0C0'
+  ];
+  let post = post || function (url, json) {
+    $.post(url, json, function (result) {
+      console.log(result);
+    }, 'application/json');
+  };
+  /**
+   * @param max = biggest possible return value
+   * @returns {number} random integer between 0 and max
+   */
+  let randomNumber = function (max) {
+    return Math.floor(Math.random() * max);
+  };
+
+
+  setInterval(function () {
+    let json = {
+      x: randomNumber(maxSize),
+      y: randomNumber(maxSize),
+      color: colors[randomNumber(colors.length)]
+    };
+    post(url, json);
+  }, intervalTime);
+}
+
